b8d14c1a634914e03ab91ed3a4f56911b85cf3e7
Grayson McGee <graysonhannam@gmail.com>
2022-03-14 04:47:14 +0000

    more port updates

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const port = 8081;
+const port = 8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
